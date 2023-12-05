6c5d307d0a8785067fe411e0ec7a93ce9ef55a6a
Deepak Singh <deepak.singh@crowdbotics.com>
2023-12-05 07:23:19 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "demomoduleapp-44862",
-  url: "https://demomoduleapp-44862.botics.co",
-  api: "https://demomoduleapp-44862.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "demomoduleapp-44862",
+  "url": "https://demomoduleapp-44862.botics.co",
+  "api": "https://demomoduleapp-44862.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
