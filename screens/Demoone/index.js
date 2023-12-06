5eb74c01ce2fbb92f814bc55d63024a7f06eec58
Deepak Singh <deepak.singh@crowdbotics.com>
2023-12-06 14:49:05 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Demoone/index.js
+++ screens/Demoone/index.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView, Text, View } from "react-native";
 
 const Demoone = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Screen Name</Text>
+      </View>
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
     }}></ScrollView>
     </SafeAreaView>;
@@ -14,7 +17,19 @@ const Demoone = () => {
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
+  },
+  header: {
+    height: 50,
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd'
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold'
   }
 });
 export default Demoone;
